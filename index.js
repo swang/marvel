90e9e90d8a05e92e4a56174a31e7c40511e51bf3
Shuan Wang <shuanwang@gmail.com>
2014-08-10 15:13:45 -0700

    Moving Resources class into its own file

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 'use strict';
 
-var request = require('request')
-  , crypto = require('crypto')
-  , plural = require('plural')
-  // , qs = require('querystring')
-
-plural.addRule(/series/i, function(w) { return w })
-
 // merge
 var merge = function(a, b) {
   var k
@@ -16,10 +9,6 @@ var merge = function(a, b) {
     }
   }
   return b
-}
-// lowercase helper
-var lower = function(s) {
-  return s.toLowerCase()
 }
 
 var responseFn = function(callback) {
@@ -63,108 +52,6 @@ Marvel.VERSION = require('./package').version
 Marvel.prototype.keys = function(priv, pub) {
   this.privateKey = priv
   this.publicKey = pub
-}
-
-var Resource = function(resource, opts) {
-  this.resource = resource
-  this.param = {}
-  this.apiDomain = opts.apiDomain
-  this.publicKey = opts.publicKey
-  this.privateKey = opts.privateKey
-}
-
-Resource.prototype.hash = function(ts) {
-  return crypto.createHash('md5').update(String(ts) + this.privateKey + this.publicKey).digest('hex')
-}
-
-Resource.prototype.get = function (cb) {
-  var uri
-    , qs
-    , ts = +new Date()
-    , uri
-
-  uri = this.apiDomain + "/" + Marvel.API_VERSION + "/public/" + lower(this.resource) //+ qs.stringify(this.param)
-  console.log(uri)
-  qs = {
-      apikey: this.publicKey
-    , ts: ts
-    , hash: this.hash(ts)
-  }
-  qs = merge(qs, this.param)
-  console.log(uri, qs)
-
-  request({
-    uri: uri,
-    qs: qs,
-    strictSSL: false
-  }, responseFn(cb))
-
-  this.param = {}
-
-}
-
-Resource.prototype.offset = function(offset) {
-  this.param.offset = offset
-  return this
-}
-
-Resource.prototype.limit = function(num) {
-  var args = arguments;
-  if (args.length === 2) {
-    this.param.offset = args[0]
-    this.param.limit = args[1]
-  }
-  else {
-    this.param.limit = args[0]
-  }
-  return this
-}
-Resource.prototype.issueNumber = function(issueNumber) {
-  this.param.issueNumber = issueNumber
-  return this
-}
-Resource.prototype.orderBy = function(orderBy) {
-  this.param.orderBy = orderBy
-  return this
-}
-
-Resource.prototype.titleStartsWith = function(titleStartsWith) {
-  this.param.titleStartsWith = titleStartsWith
-  return this
-}
-
-Resource.prototype.title = function(title) {
-  this.param.title = title
-  return this
-}
-
-Resource.prototype.startYear = function(startYear) {
-  this.param.startYear = startYear
-  return this
-}
-
-Resource.prototype.nameStartsWith = function(nameStartsWith) {
-  this.param.nameStartsWith = nameStartsWith
-  return this
-}
-
-Resource.prototype.name = function(name) {
-  this.param.name = name
-  return this
-}
-
-var resources = ['Comic', 'Character', 'Creator', 'Event', 'Series', 'Story']
-
-for (var i = 0; i < resources.length; i++) {
-  var addResource = function(rsrc) {
-    rsrc = plural(lower(rsrc))
-    Resource.prototype[rsrc] = function(queryVal) {
-      if (this.resource !== rsrc) {
-        this.param[rsrc] = queryVal
-      }
-      return this
-    }
-  }(resources[i])
 }
 
 module.exports = Marvel
