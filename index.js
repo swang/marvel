a29b49669b1e0a534ec0a6b5def06fd3eac1e862
Shuan Wang <shuanwang@gmail.com>
2014-08-10 11:05:15 -0700

    Moving over new index to index.js

--- index.js
+++ index.js
@@ -3,128 +3,168 @@
 var request = require('request')
   , crypto = require('crypto')
   , plural = require('plural')
+  // , qs = require('querystring')
 
 plural.addRule(/series/i, function(w) { return w })
 
+// merge
+var merge = function(a, b) {
+  var k
+  for (k in a) {
+    if (a.hasOwnProperty(k) && b.hasOwnProperty(k) === false) {
+      b[k] = a[k]
+    }
+  }
+  return b
+}
+// lowercase helper
+var lower = function(s) {
+  return s.toLowerCase()
+}
+
+var responseFn = function(callback) {
+  return function (err, resp, body) {
+    if (err) { return callback(err) }
+    if (resp.statusCode > 400 && resp.statusCode < 500) {
+      return callback(JSON.parse(body))
+    }
+    return callback(err, JSON.parse(body))
+  }
+}
+
+
 var Marvel = function(opts) {
+  var defaults = {
+    apiDomain: "https://gateway.marvel.com"
+  , gzip: true
+  }
+
+  opts = merge(defaults, opts || {})
+
   if (opts.privateKey === undefined || opts.publicKey === undefined) {
     throw new Error('Unable to create a hash because of missing privateKey/publicKey')
   }
+
+  this.param = {}
   this.publicKey = opts.publicKey
   this.privateKey = opts.privateKey
-  this.apiDomain = "https://gateway.marvel.com"
-}
+  this.apiDomain = opts.apiDomain
+  this.gzip = opts.gzip
 
-var resources = ['Comic', 'Character', 'Creator', 'Event', 'Series', 'Story']
-  , responseFn
+  this.characters = new Resource('characters', opts)
+  console.log(Object.keys(this.characters))
 
-responseFn = function(callback) {
-  return function (err, resp, body) {
-    if (!err && resp.statusCode === 200) {
-      return callback(err, JSON.parse(body))
-    }
-    if (resp.statusCode === 429) { // Rate-limit exceeded
-      //'{"code":"RequestThrottled","message":"You have exceeded your rate limit.  Please try again later."}'
-      return callback(JSON.parse(body))
-    }
-    return callback(err)
-  }
 }
 
 Marvel.API_VERSION = "v1"
 Marvel.VERSION = require('./package').version
 
-resources.forEach(function(res) {
-  var methodName = "get" + plural(res)
 
-  Marvel.prototype[methodName] = function(params, callback) {
-    var attr
-      , qs
-      , ts = +new Date()
-      , uri
-
-    qs = {
-        apikey: this.publicKey
-      , ts: ts
-      , hash: this.getHash(ts)
-    }
-
-    for (attr in params) {
-      if (params.hasOwnProperty(attr)) {
-        qs[attr] = params[attr]
-      }
-    }
-
-    uri = this.apiDomain + "/" + Marvel.API_VERSION + "/public/" + plural(res).toLowerCase()
-
-    request({
-      uri: uri,
-      qs: qs,
-      strictSSL: false
-    }, responseFn(callback))
-
-  }
-
-  resources.forEach(function(altRes) {
-    var methodNameFromProp
-    if (res !== altRes) {
-      methodNameFromProp = "get" + plural(res) + "By" + altRes + "Id"
-    }
-    else {
-      methodNameFromProp = "get" + plural(altRes) + "ById"
-    }
-    Marvel.prototype[methodNameFromProp] = function() {
-      var args = Array.prototype.slice.call(arguments)
-        , attr
-        , callback
-        , paramId
-        , params
-        , qs
-        , ts = +new Date()
-        , uri
-
-      if (args.length === 2) {
-        paramId = args[0]
-        callback = args[1]
-      }
-      if (args.length === 3) {
-        paramId = args[0]
-        params = args[1]
-        callback = args[2]
-      }
-      qs = {
-          apikey: this.publicKey
-        , ts: ts
-        , hash: this.getHash(ts)
-      }
-      qs[altRes.toLowerCase() + "Id"] = paramId
-
-      for (attr in params) {
-        if (params.hasOwnProperty(attr)) {
-          qs[attr] = params[attr]
-        }
-      }
-      uri = this.apiDomain + "/" + Marvel.API_VERSION + "/public/" + plural(altRes).toLowerCase() + "/" + paramId + ((res !== altRes) ? ("/" + plural(res).toLowerCase()) : "")
-
-      request({
-        uri: uri,
-        qs: qs,
-        strictSSL: false
-      }, responseFn(callback))
-    }
-  })
-})
-
-Marvel.prototype.setPublicKey = function(key) {
-  this.publicKey = key;
+Marvel.prototype.keys = function(priv, pub) {
+  this.privateKey = priv
+  this.publicKey = pub
 }
 
-Marvel.prototype.setPrivateKey = function(key) {
-  this.privateKey = key;
+var Resource = function(resource, opts) {
+  this.resource = resource
+  this.param = {}
+  this.apiDomain = opts.apiDomain
+  this.publicKey = opts.publicKey
+  this.privateKey = opts.privateKey
 }
 
-Marvel.prototype.getHash = function(ts) {
+Resource.prototype.hash = function(ts) {
   return crypto.createHash('md5').update(String(ts) + this.privateKey + this.publicKey).digest('hex')
+}
+
+Resource.prototype.get = function (cb) {
+  var uri
+    , qs
+    , ts = +new Date()
+    , uri
+
+  uri = this.apiDomain + "/" + Marvel.API_VERSION + "/public/" + lower(this.resource) //+ qs.stringify(this.param)
+  console.log(uri)
+  qs = {
+      apikey: this.publicKey
+    , ts: ts
+    , hash: this.hash(ts)
+  }
+  qs = merge(qs, this.param)
+  console.log(uri, qs)
+
+  request({
+    uri: uri,
+    qs: qs,
+    strictSSL: false
+  }, responseFn(cb))
+
+  this.param = {}
+
+}
+
+Resource.prototype.offset = function(offset) {
+  this.param.offset = offset
+  return this
+}
+
+Resource.prototype.limit = function(num) {
+  var args = arguments;
+  if (args.length === 2) {
+    this.param.offset = args[0]
+    this.param.limit = args[1]
+  }
+  else {
+    this.param.limit = args[0]
+  }
+  return this
+}
+Resource.prototype.issueNumber = function(issueNumber) {
+  this.param.issueNumber = issueNumber
+  return this
+}
+Resource.prototype.orderBy = function(orderBy) {
+  this.param.orderBy = orderBy
+  return this
+}
+
+Resource.prototype.titleStartsWith = function(titleStartsWith) {
+  this.param.titleStartsWith = titleStartsWith
+  return this
+}
+
+Resource.prototype.title = function(title) {
+  this.param.title = title
+  return this
+}
+
+Resource.prototype.startYear = function(startYear) {
+  this.param.startYear = startYear
+  return this
+}
+
+Resource.prototype.nameStartsWith = function(nameStartsWith) {
+  this.param.nameStartsWith = nameStartsWith
+  return this
+}
+
+Resource.prototype.name = function(name) {
+  this.param.name = name
+  return this
+}
+
+var resources = ['Comic', 'Character', 'Creator', 'Event', 'Series', 'Story']
+
+for (var i = 0; i < resources.length; i++) {
+  var addResource = function(rsrc) {
+    rsrc = plural(lower(rsrc))
+    Resource.prototype[rsrc] = function(queryVal) {
+      if (this.resource !== rsrc) {
+        this.param[rsrc] = queryVal
+      }
+      return this
+    }
+  }(resources[i])
 }
 
 module.exports = Marvel
