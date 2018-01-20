a95ef036ab2b1dc7d8efa9b9e03d66c236fd1fc8
Shuan Wang <shuanwang@gmail.com>
2018-01-19 23:58:55 -0800

    switch linter to eslint

--- test/index.js
+++ test/index.js
@@ -7,12 +7,12 @@ describe('Marvel', function() {
   describe('constructor', function() {
     it('should throw an error when public/private keys are missing', function() {
       try {
-        new Marvel()
+        var x = new Marvel()
       }
       catch (e) {
         return e.should.be.an.Error
       }
-      should(false).ok
+      should(false).ok()
     })
   })
 })
